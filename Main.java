ee0458418aa1c6ab3505bc6ceedef7a7a41821cf
VinitR1212 <154247095+VinitR1212@users.noreply.github.com>
2024-04-26 00:32:48 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,61 @@
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    Rectangle rectangle = new Rectangle(4, 6);
-    System.out.println("Rectangle Area: " + rectangle.calculateArea());
-    System.out.println("Rectangle Perimeter: " + rectangle.calculatePerimeter());
+    Scanner scanner = new Scanner(System.in);
 
-    Parallelogram parallelogram = new Parallelogram(4, 5);
-    System.out.println("\nParallelogram Area: " + parallelogram.calculateArea());
-    System.out.println("Parallelogram Perimeter: " + parallelogram.calculatePerimeter());
+    System.out.println("Select a shape to calculate:");
+    System.out.println("1. Rectangle");
+    System.out.println("2. Parallelogram");
+    System.out.println("3. Square");
+    System.out.println("4. Equilateral Triangle");
+    int choice = scanner.nextInt();
 
-    Square square = new Square(5);
-    System.out.println("\nSquare Area: " + square.calculateArea());
-    System.out.println("Square Perimeter: " + square.calculatePerimeter());
+    double[] dimensions;
+    switch (choice) {
+      case 1:
+        dimensions = getDimensions("length", "width");
+        Rectangle rectangle = new Rectangle(dimensions[0], dimensions[1]);
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.calculatePerimeter());
+        break;
+      case 2:
+        dimensions = getDimensions("base", "height");
+        Parallelogram parallelogram = new Parallelogram(dimensions[0], dimensions[1]);
+        System.out.println("Parallelogram Area: " + parallelogram.calculateArea());
+        System.out.println("Parallelogram Perimeter: " + parallelogram.calculatePerimeter());
+        break;
+      case 3:
+        double side = getSingleDimension("side");
+        Square square = new Square(side);
+        System.out.println("Square Area: " + square.calculateArea());
+        System.out.println("Square Perimeter: " + square.calculatePerimeter());
+        break;
+      case 4:
+        side = getSingleDimension("side");
+        Triangle triangle = new Triangle(side);
+        System.out.println("Equilateral Triangle Area: " + triangle.calculateArea());
+        System.out.println("Equilateral Triangle Perimeter: " + triangle.calculatePerimeter());
+        break;
+      default:
+        System.out.println("Invalid choice!");
+    }
 
-    Triangle triangle = new Triangle(6);
-    System.out.println("\nEquilateral Triangle Area: " + triangle.calculateArea());
-    System.out.println("Equilateral Triangle Perimeter: " + triangle.calculatePerimeter());
+    scanner.close();
+  }
+
+  private static double[] getDimensions(String... dimensionNames) {
+    Scanner scanner = new Scanner(System.in);
+    double[] dimensions = new double[dimensionNames.length];
+    for (int i = 0; i < dimensionNames.length; i++) {
+      System.out.print("Enter " + dimensionNames[i] + ": ");
+      dimensions[i] = scanner.nextDouble();
+    }
+    return dimensions;
+  }
+
+  private static double getSingleDimension(String dimensionName) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter " + dimensionName + ": ");
+    return scanner.nextDouble();
   }
 }
